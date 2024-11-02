870c6274b4365b02215a3a4383afdc327ee146f3
ADong23-star <kuv0194@gmail.com>
2024-11-03 01:51:42 +0700

    Add files via upload
    create card

---
+++ 4.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Mùa Thị Xoan';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = 'Gửi tới Mùa Thị Xoan';
+const contentCard = 'Chúc em tuổi mới tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'https://scontent.fhan15-2.fna.fbcdn.net/v/t39.30808-6/461796266_561234002998934_8144418748585242653_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHqIH-bsGkrzObE_w-vOI4iBh5p2fZWa9gGHmnZ9lZr2L4P8l7Rr5ryQIW7z2-KhjRssVni1RKT-xSJ2xI9zw5Z&_nc_ohc=tWlwA40E1dsQ7kNvgHby8l0&_nc_zt=23&_nc_ht=scontent.fhan15-2.fna&_nc_gid=Az-v3ZYDgQ-uAvOm4x-yRIA&oh=00_AYDK4Emg5_VoFE7eWjyh3gw8VN42RDnLocq6vdpwxc2hng&oe=672C0566';
+const base64 = 'https://scontent.fhan15-2.fna.fbcdn.net/v/t39.30808-6/461796266_561234002998934_8144418748585242653_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHqIH-bsGkrzObE_w-vOI4iBh5p2fZWa9gGHmnZ9lZr2L4P8l7Rr5ryQIW7z2-KhjRssVni1RKT-xSJ2xI9zw5Z&_nc_ohc=tWlwA40E1dsQ7kNvgHby8l0&_nc_zt=23&_nc_ht=scontent.fhan15-2.fna&_nc_gid=Az-v3ZYDgQ-uAvOm4x-yRIA&oh=00_AYDK4Emg5_VoFE7eWjyh3gw8VN42RDnLocq6vdpwxc2hng&oe=672C0566';
+const giftImageBase64 = "data:image/png;base64, " + base64;
